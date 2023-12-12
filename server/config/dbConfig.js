12acec82b78c5dd427a7fb9ec283ee14d05068b5
Juan Pablo <juanpablojustinianoorosco@gmail.com>
2023-12-12 17:04:33 -0400

    primer commit

---
+++ server/config/dbConfig.js
@@ -0,0 +1,43 @@
+// dbConfig.js
+import { Sequelize } from 'sequelize';
+import {db_HOST,db_NAME,db_PASSWORD,db_USER,db_PORT} from '../src/config.js'
+const dbConfig = {
+    HOST: db_HOST,
+    USER: db_USER,
+    PASSWORD: db_PASSWORD,
+    DB: db_NAME,
+    dialect: 'mysql',
+    
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+        
+    },
+    dialectOptions: {
+        charset: 'utf8',
+    },
+};
+
+const sequelize = new Sequelize(
+    dbConfig.DB,
+    dbConfig.USER,
+    dbConfig.PASSWORD,
+    {
+        host: dbConfig.HOST,
+        dialect: dbConfig.dialect,
+        pool: {
+            max: dbConfig.pool.max,
+            min: dbConfig.pool.min,
+            acquire: dbConfig.pool.acquire,
+            idle: dbConfig.pool.idle,
+        },
+        dialectOptions: {
+            charset: dbConfig.dialectOptions.charset,
+        },
+    }
+);
+
+export { sequelize }; 
+export default dbConfig;
